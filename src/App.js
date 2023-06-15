cdf16e1daa4b7475990a6286e461442b2cdb7749
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-06-15 07:52:50 +0300

    Implement high-order component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
-function App() {
-  return <div className="App"></div>;
+import React, { Component } from "react";
+import updateObjectUsingPath from "./Utils/set";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      phoneNumber: "",
+      education: [],
+      work: [],
+    };
+  }
+  // that updates any nested property inside the state with the provided value
+  updateStateProperty = (path, value) => {
+    this.setState((prevState) => updateObjectUsingPath(prevState, path, value));
+  };
+  newEducation = () => {
+    this.state.education.push({});
+  };
+
+  newWork = () => {
+    this.state.work.push({});
+  };
+
+  render() {
+    return <div></div>;
+  }
 }
 
 export default App;
