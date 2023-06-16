74b0df426575d345beb37bc379718972594ba17e
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-06-16 08:18:17 +0300

    Add header, remove useless div

--- src/Components/generalInfo.js
+++ src/Components/generalInfo.js
@@ -5,12 +5,11 @@ export default class GeneralInfo extends Component {
   render() {
     const { updateFunction } = this.props;
     return (
-      <div className="inputs">
-        <div className="general-info">
-          <Input path={["name"]} updateFunction={updateFunction}></Input>
-          <Input path={["email"]} updateFunction={updateFunction}></Input>
-          <Input path={["phoneNumber"]} updateFunction={updateFunction}></Input>
-        </div>
+      <div className="general-info">
+        <h1>General Info</h1>
+        <Input path={["name"]} updateFunction={updateFunction}></Input>
+        <Input path={["email"]} updateFunction={updateFunction}></Input>
+        <Input path={["phoneNumber"]} updateFunction={updateFunction}></Input>
       </div>
     );
   }
