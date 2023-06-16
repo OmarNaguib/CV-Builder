0cc0622c6cdbb56d4ca32be4a1b8cbe62a9db825
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-06-16 08:27:10 +0300

    Separate and Pascal Case labels

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -3,9 +3,13 @@ import React, { Component } from "react";
 export default class Input extends Component {
   render() {
     const { path, updateFunction } = this.props;
+    const label = path[path.length - 1]
+      .split(/(?=[A-Z])/)
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
     return (
       <div className="input">
-        <label htmlFor={path.join("-")}>{path[path.length - 1] + ": "}</label>
+        <label htmlFor={path.join("-")}>{label + ": "}</label>
         <input
           id={path.join("-")}
           type="text"
