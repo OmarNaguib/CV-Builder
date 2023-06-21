e5ce40fcc31a05cbdc482b1dbec4d96e408ac632
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-06-21 14:57:47 +0300

    Migrate Inputs

--- src/Components/input/Inputs.js
+++ src/Components/input/Inputs.js
@@ -1,26 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import GeneralInfo from "./generalInfo";
 import Education from "./Education";
 import Work from "./Work";
 import "../../styles/Inputs.css";
 
-export default class Inputs extends Component {
-  render() {
-    const { updateFunction, state, newEducation, newWork } = this.props;
-    return (
-      <div className="inputs">
-        <GeneralInfo updateFunction={updateFunction} data={state}></GeneralInfo>
-        <Education
-          data={state}
-          updateFunction={updateFunction}
-          newEducation={newEducation}
-        ></Education>
-        <Work
-          data={state}
-          updateFunction={updateFunction}
-          newWork={newWork}
-        ></Work>
-      </div>
-    );
-  }
+export default function Inputs(props) {
+  const { updateFunction, state, newEducation, newWork } = props;
+  return (
+    <div className="inputs">
+      <GeneralInfo updateFunction={updateFunction} data={state}></GeneralInfo>
+      <Education
+        data={state}
+        updateFunction={updateFunction}
+        newEducation={newEducation}
+      ></Education>
+      <Work
+        data={state}
+        updateFunction={updateFunction}
+        newWork={newWork}
+      ></Work>
+    </div>
+  );
 }
