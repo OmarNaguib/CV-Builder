7c7c76610adde904a58300556f8160e6133445d4
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-06-21 14:46:13 +0300

    Rewrite Input ,AreaInput using functional components

--- src/Components/input/AreaInput.js
+++ src/Components/input/AreaInput.js
@@ -1,26 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import get from "../../Utils/get";
 
-export default class AreaInput extends Component {
-  render() {
-    const { path, updateFunction, data } = this.props;
-    const value = get(data, path);
-    const label = path[path.length - 1]
-      .split(/(?=[A-Z])/)
-      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(" ");
+export default function AreaInput(props) {
+  const { path, updateFunction, data } = props;
+  const value = get(data, path);
+  const label = path[path.length - 1]
+    .split(/(?=[A-Z])/)
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
 
-    return (
-      <div className="input">
-        <label htmlFor={path.join("-")}>{label + ": "}</label>
-        <textarea
-          id={path.join("-")}
-          onBlur={(e) => {
-            updateFunction(path, e.target.value);
-          }}
-          defaultValue={value}
-        ></textarea>
-      </div>
-    );
-  }
+  return (
+    <div className="input">
+      <label htmlFor={path.join("-")}>{label + ": "}</label>
+      <textarea
+        id={path.join("-")}
+        onBlur={(e) => {
+          updateFunction(path, e.target.value);
+        }}
+        defaultValue={value}
+      ></textarea>
+    </div>
+  );
 }
--- src/Components/input/Input.js
+++ src/Components/input/Input.js
@@ -1,26 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import get from "../../Utils/get";
 
-export default class Input extends Component {
-  render() {
-    const { path, updateFunction, data } = this.props;
-    const value = get(data, path);
-    const label = path[path.length - 1]
-      .split(/(?=[A-Z])/)
-      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(" ");
-    return (
-      <div className="input">
-        <label htmlFor={path.join("-")}>{label + ": "}</label>
-        <input
-          id={path.join("-")}
-          type="text"
-          onChange={(e) => {
-            updateFunction(path, e.target.value);
-          }}
-          defaultValue={value}
-        />
-      </div>
-    );
-  }
+export default function Input(props) {
+  const { path, updateFunction, data } = props;
+  const value = get(data, path);
+  const label = path[path.length - 1]
+    .split(/(?=[A-Z])/)
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+  return (
+    <div className="input">
+      <label htmlFor={path.join("-")}>{label + ": "}</label>
+      <input
+        id={path.join("-")}
+        type="text"
+        onChange={(e) => {
+          updateFunction(path, e.target.value);
+        }}
+        defaultValue={value}
+      />
+    </div>
+  );
 }
