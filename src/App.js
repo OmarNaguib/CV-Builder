8b9a0f8277290d8c1fc2ba0aa5f0e82f821f2273
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-06-21 15:17:15 +0300

    Migrate App to functional component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import updateObjectUsingPath from "./Utils/set";
 import Inputs from "./Components/input/Inputs";
 import Display from "./Components/display/Display";
 import "./styles/App.css";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      email: "",
-      phoneNumber: "",
-      education: [],
-      work: [],
-    };
-  }
+function App(props) {
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    phoneNumber: "",
+    education: [],
+    work: [],
+  });
+  // console.log("state:", state);
   // that updates any nested property inside the state with the provided value
-  updateStateProperty = (path, value) => {
-    this.setState((prevState) => updateObjectUsingPath(prevState, path, value));
-    console.log(this.state);
+  const updateStateProperty = (path, value) => {
+    setState((prevState) => {
+      return { ...updateObjectUsingPath(prevState, path, value) };
+    });
+    console.log("state update", state);
   };
-  newEducation = () => {
-    this.setState((prevState) => {
+  const newEducation = () => {
+    setState((prevState) => {
       return {
+        ...prevState,
         education: prevState.education.concat({
           schoolName: "",
           studyType: "",
@@ -30,12 +31,14 @@ class App extends Component {
         }),
       };
     });
-    return this.state.education.length;
+    return state.education.length;
   };
 
-  newWork = () => {
-    this.setState((prevState) => {
+  const newWork = () => {
+    setState((prevState) => {
+      console.log(prevState);
       return {
+        ...prevState,
         work: prevState.work.concat({
           company: "",
           position: "",
@@ -45,15 +48,13 @@ class App extends Component {
         }),
       };
     });
-    return this.state.work.length;
+    return state.work.length;
   };
 
-  buttonClick = (e) => {
-    console.log("here");
+  const buttonClick = (e) => {
     const inputs = document.querySelector(".inputs");
 
     const display = document.querySelector(".display");
-    console.log(display);
     if (e.target.textContent === "Submit") {
       inputs.classList.add("hidden");
       display.classList.remove("hidden");
@@ -65,23 +66,21 @@ class App extends Component {
     }
   };
 
-  render() {
-    return (
-      <>
-        <header>CV Builder</header>
-        <div className="app">
-          <Inputs
-            updateFunction={this.updateStateProperty}
-            newWork={this.newWork}
-            newEducation={this.newEducation}
-            state={this.state}
-          ></Inputs>
-          <Display data={this.state}></Display>
-          <button onClick={this.buttonClick}>Submit</button>
-        </div>
-      </>
-    );
-  }
+  return (
+    <>
+      <header>CV Builder</header>
+      <div className="app">
+        <Inputs
+          updateFunction={updateStateProperty}
+          newWork={newWork}
+          newEducation={newEducation}
+          state={state}
+        ></Inputs>
+        <Display data={state}></Display>
+        <button onClick={buttonClick}>Submit</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
